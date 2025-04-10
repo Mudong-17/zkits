9ed369242828601c67dee20e86ef7b11f3405c52
暮冬拾柒 <zhoubaijun17@outlook.com>
2025-04-10 10:51:37 +0800

    新增 @zkits/prettier 包及其相关配置，包含基本的 prettier 配置文件和插件依赖，同时新增 @zkits/tsconfig 包以提供 TypeScript 配置。

---
+++ packages/prettier/src/base.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'prettier';
+
+const base: Config = {
+  plugins: [
+    'prettier-plugin-packagejson',
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-prisma',
+    'prettier-plugin-multiline-arrays',
+    'prettier-plugin-tailwindcss',
+  ],
+  printWidth: 120,
+  proseWrap: 'never',
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+export default base;
---
+++ packages/tsconfig/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @zkits/tsconfig
+
+## 1.0.0
+
+### Major Changes
+
+- 新增 @zkits/prettier 包及其相关配置，包含基本的 prettier 配置文件和插件依赖，同时新增 @zkits/tsconfig 包以提供 TypeScript 配置。
